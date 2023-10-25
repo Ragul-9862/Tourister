0dcf9f696734f3d47d31f00e02cd802411b33588
ragul-9862 <ragulragul9862@gmail.com>
2023-10-25 10:33:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter ,Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Login from './Components/Login';
 import Signup from './Components/Signup';
-import { auth } from './Firebase/Firebase';
+
 
 function App() {
-  const [username,setUsername] = useState("")
-  useEffect(()=>{
-    auth.onAuthStateChanged((user)=>{
-      if(user){
-        setUsername(user.displayName)
-      }else{
-        setUsername("")
-      }
-      console.log(user);
-    })
-  })
+  
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
         </Routes>
       </BrowserRouter>
